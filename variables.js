f62cce41bd7a895999a09d38a221b4471f4e4b51
John Crites <johncritesdev@gmail.com>
2021-11-01 14:56:29 -0700

    ...

---
+++ variables.js
@@ -0,0 +1,6 @@
+const numOfPeople = 1;
+//semantic convention -- should be using by default
+const num2 = 2;
+console.log(numOfPeople * 2);
+
+//you want the variable name to resembe the value, avoid letters only
