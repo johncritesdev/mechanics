876b3d83ba78de1de2f95c4d105a601079bcfad4
John Crites <johncritesdev@gmail.com>
2021-11-06 15:29:31 -0700

    ...

---
+++ strings.js
@@ -0,0 +1,14 @@
+let myName = "John";
+let myMiddleName = "Jack";
+
+let myFriendsName = 'Mark';
+//interpolation
+let myFriendsFullName = `${myFriendsName} Rogers`;
+console.log(myFriendsFullName);
+//indexing, look at each character in your string
+let myName1 = "Jay";
+console.log(myName1[0]);
+//concatenation
+console.log(myFriendsName + ' ' + myMiddleName);
+//length method
+console.log(myName.length);
