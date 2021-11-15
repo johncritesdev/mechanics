6c7571e04d08d6fce9dbbb8ef8c316722554e55c
John Crites <johncritesdev@gmail.com>
2021-11-15 13:22:53 -0800

    ...

---
+++ compound-data-types/objects.js
@@ -0,0 +1,34 @@
+//objects
+
+const user = {
+  name: "jay",
+  age: 31,
+  employed: true,
+  socialMedia: {
+    twitter: "",
+    fb: "",
+    github: ""
+  }
+};
+
+//dot syntax
+console.log(user.name);
+console.log(user.age);
+console.log(user.employed);
+
+//string literal indexing
+console.log(user['name']);
+console.log(user['age']);
+console.log(user['employed']);
+
+//variable syntax
+const key = 'name'
+
+console.log(user[key]);
+
+//change
+user.age = 28
+console.log(user.age);
+
+//chaining
+console.log(user.socialMedia.twitter);
