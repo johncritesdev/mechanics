6b4ab09959a65e86cea756c5f8bbedfdbeef84bb
John Crites <johncritesdev@gmail.com>
2021-12-03 14:11:25 -0800

    ...

---
+++ conditional-chaining.js
@@ -0,0 +1,20 @@
+//conditional chaining
+
+let num = 0;
+let firstName = "Jay";
+
+//logical & operator -- both must be true
+if (num === 0 && firstName === "Jay") {
+  console.log("this is a truthy");
+} else {
+  console.log("this is a falsey");
+}
+//logical or operator -- at least one must be true
+if (num === 1 || firstName === "Jay") {
+  console.log("this is a truthy");
+} else {
+  console.log("this is a falsey");
+}
+
+
+
